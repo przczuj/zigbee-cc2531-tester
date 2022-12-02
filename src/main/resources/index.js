9e45e4a9dedc72e7814b1dce9c9c26576d6ea8ce
Przemyslaw Czuj <przczuj@gmail.com>
2022-12-02 17:45:14 +0100

    Remove periodic light switch from the tester

--- src/main/resources/index.js
+++ src/main/resources/index.js
@@ -10,34 +10,11 @@ const coordinator = new Controller({
   databasePath: DB
 })
 
-function switchLight(ieeeAddr, state) {
-  const lightSwitch = coordinator.getDeviceByIeeeAddr(ieeeAddr)
-  console.log('Light Switch: ' + JSON.stringify(lightSwitch))
-
-  const endpoint = lightSwitch.getEndpoint(1)
-  console.log('Light Switch endpoint: ' + JSON.stringify(endpoint))
-
-  if (state) {
-    endpoint.command(6, 1)
-  } else {
-    endpoint.command(6, 0)
-  }
-}
-
-var state = false
-setInterval(function() { switchLight('0xa4c1389f36bcf406', state); state = !state }, 4000)
-
 coordinator.on('message', async (msg) => {
   console.log(msg)
 })
 
-coordinator
-  .start((err) => {
-    if (err) {
-      console.error(err)
-      process.exit(1)
-    }
-  })
+coordinator.start()
   .then(async () => {
     console.log('started with device', SERIAL)
     coordinator.permitJoin(600, (err) => {
