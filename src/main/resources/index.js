776c6801e618ecb31e1548c684b427162bd1fee3
Przemyslaw Czuj <przczuj@gmail.com>
2023-01-13 19:15:07 +0100

    Introduce HTTP server to make zigbee operations ad-hoc

--- src/main/resources/index.js
+++ src/main/resources/index.js
@@ -1,26 +1,75 @@
 // Current implementation taken from example:
 //  https://github.com/Koenkk/zigbee-herdsman/blob/master/examples/join-and-log.js
-const { Controller } = require('zigbee-herdsman')
+const { Controller } = require('zigbee-herdsman');
+const express = require('express');
+const fs = require('fs');
 
-const SERIAL = '/dev/ttyACM0'
-const DB = './devices.db'
+const SERIAL = '/dev/ttyACM0';
+const DB = './devices.db';
+const PORT = 8080;
 
 const coordinator = new Controller({
   serialPort: { path: SERIAL },
   databasePath: DB
-})
+});
 
 coordinator.on('message', async (msg) => {
-  console.log(`type ${msg.type}; devID ${msg.device.ID}; endpointID ${msg.endpoint.ID}; profileID ${msg.endpoint.profileID}; groupID ${msg.groupID}; cluster ${msg.groupID}; linkquality ${msg.linkquality}; data ${msg.data.toString('hex')}`)
-})
+  console.log(`type ${msg.type}; devID ${msg.device.ID}; endpointID ${msg.endpoint.ID}; profileID ${msg.endpoint.profileID}; groupID ${msg.groupID}; cluster ${msg.groupID}; linkquality ${msg.linkquality}; data ${msg.data.toString('hex')}`);
+});
 
 coordinator.start()
   .then(async () => {
-    console.log('started with device', SERIAL)
+    console.log('started with device', SERIAL);
     coordinator.permitJoin(600, (err) => {
       if (err) {
-        console.error(err)
+        console.error(err);
       }
-    })
-  })
+    });
+  });
 
+function getDatabaseContent() {
+  const rawJsonlDb = fs.readFileSync(DB, 'utf8');
+  const parsedJsonlDb = rawJsonlDb.split('\n').map(line => JSON.parse(line));
+  const prettyJsonDb = JSON.stringify(parsedJsonlDb, null, 4);
+
+  return prettyJsonDb;
+}
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send(getDatabaseContent());
+});
+
+app.get('/device/:ieeeAddr/endpoint/:endpoint/cluster/:clusterKey/attribute/:attributeId', function (req, res) {
+  coordinator
+    .getDeviceByIeeeAddr(req.params.ieeeAddr)
+    .getEndpoint(req.params.endpoint)
+    .read(req.params.clusterKey, [req.params.attributeId]);
+
+  res.send(getDatabaseContent());
+});
+
+app.get('/device/:ieeeAddr/endpoint/:endpoint/cluster/:clusterKey/attribute/:attributeId/value/:attributeValue', function (req, res) {
+  const attributes = {};
+  attributes[req.params.attributeId] = req.params.attributeValue;
+  coordinator
+    .getDeviceByIeeeAddr(req.params.ieeeAddr)
+    .getEndpoint(req.params.endpoint)
+    .write(req.params.clusterKey, attributes);
+
+  res.send(getDatabaseContent());
+});
+
+app.get('/device/:ieeeAddr/endpoint/:endpoint/cluster/:clusterKey/command/:commandKey', function (req, res) {
+  coordinator
+    .getDeviceByIeeeAddr(req.params.ieeeAddr)
+    .getEndpoint(req.params.endpoint)
+    .command(req.params.clusterKey, req.params.commandKey, JSON.parse(req.query.payload));
+
+  res.send(getDatabaseContent());
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
